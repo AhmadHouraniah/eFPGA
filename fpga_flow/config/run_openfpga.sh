579c075c438dec5f86f438d0134576ca1b3923b4
Ahmad Houraniah <ahmad.houraniah@yongatek.com>
2025-10-06 12:51:56 +0300

    Update FPGA flow config

--- fpga_flow/config/run_openfpga.sh
+++ fpga_flow/config/run_openfpga.sh
@@ -1,2 +1,2 @@
-docker run -it --rm --user $UID -v /home/ahouraniah/efpga-design-flow:/home/openfpga_user/ ghcr.io/lnis-uofu/openfpga-master:latest /home/openfpga_user/docker_entrypoint.sh
+docker run -it --rm --user $UID -v /home/ahouraniah/eFPGA:/home/openfpga_user/ ghcr.io/lnis-uofu/openfpga-master:3b3acf31c /home/openfpga_user/docker_entrypoint.sh
 
